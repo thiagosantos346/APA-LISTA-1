db328629e8af1dd87677ecbcccf32dce2aaf6917
Thiago Silva <thiagosantos346@gmail.com>
2020-10-04 22:57:21 -0300

    Change code to report the number of instruction to run.

--- ex1/main.c
+++ ex1/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-#define PQ_SIZE 2
+#define PQ_SIZE 1000
 
-typedef struct {
-    int q[PQ_SIZE+1];
+typedef struct
+{
+    int q[PQ_SIZE + 1];
     int n;
 } priority_queue;
 /* body of queue */
 /* number of queue elements */
 
-void pq_swap(priority_queue *priority_queue, int n, int key );
+void pq_swap(priority_queue *priority_queue, int n, int key);
 
-void swap(int *s, int n, int key );
+void swap(int *s, int n, int key);
 
 int pq_parent(int n);
 
@@ -24,123 +25,101 @@ void bubble_up(priority_queue *q, int p);
 
 void pq_init(priority_queue *q);
 
-void make_heap(priority_queue *q, int s[], int n);
+void make_heap(priority_queue *q, int s[], int n, int *operations);
 
-int extract_min(priority_queue *q);
+int extract_min(priority_queue *q, int *operations);
 
-void bubble_down(priority_queue *q, int p);
+int bubble_down(priority_queue *q, int p, int operations);
 
-void heapsort(int s[], int n);
+int heapsort(int s[], int n);
 
-int simpleFindeMinimum(int * s, int n,  int *operations);
+int simpleFindeMinimum(int *s, int n, int *operations);
 
-int deleteMinimum(int * s, int start, int end, int minimum );
+int deleteMinimum(int *s, int start, int end, int minimum);
 
-int SelectionSort(int * s, int * sortedS, int n);
+int SelectionSort(int *s, int *sortedS, int n);
 
 int heap_compare(priority_queue *q, int i, int count, int x);
 
-void main( int agrc, char *argv[]){
+void main(int agrc, char *argv[])
+{
 
-    
-    int t = 10;
+    int t = PQ_SIZE;
     int vetor[t];
     int nVetor[t];
     int vVetor[t];
 
-    for(int i = 0; i < t; ++i){
-        vetor[i] = (t - i); // the hard way.
+    for (int i = 0; i < t; ++i)
+    {
+        vetor[i] = (t - i);  // the hard way.
         nVetor[i] = (t - i); // the hard way.
         vVetor[i] = (t - i); // the hard way.
     }
-    
-    
-    
-    heapsort(vetor, t);
 
     printf(
-        "Number of operarions to sort a vector with size equals to %d is %d.", 
+        "Selection Sort(Hard way) : Number of operarions to sort a vector with size equals to %d is %d.\n",
         t,
-        SelectionSort(vetor, nVetor, t)
-    );
+        SelectionSort(vetor, nVetor, t));
 
+    printf(
+        "Heap Sort(SelectionSort Otimized) : Number of operarions to sort a vector with size equals to %d is %d.\n",
+        t,
+        heapsort(vetor, t));
 }
 
-
-void myfuncn( int *var1, int var2)
+void pq_swap(priority_queue *priority_queue, int i, int min_index)
 {
-	/* The pointer var1 is pointing to the first element of
-	 * the array and the var2 is the size of the array. In the
-	 * loop we are incrementing pointer so that it points to
-	 * the next element of the array on each increment.
-	 *
-	 */
-    for(int x=0; x<var2; x++)
-    {
-        printf("Value of var_arr[%d] is: %d \n", x, *var1);
-        /*increment pointer for next element fetch*/
-        var1++;
-    }
-}
-
-int bmain()
-{
-    int var_arr[] = {11, 22, 33, 44, 55, 66, 77};
-    myfuncn(var_arr, 7);
-    return 0;
-}
-
-//q, p, min_index
-void pq_swap(priority_queue *priority_queue, int i, int min_index ){
     int swapVar;
 
     swapVar = (int)priority_queue->q[i];
-    priority_queue->q[i]    = priority_queue->q[min_index];
-    priority_queue->q[min_index]  = swapVar;
-
+    priority_queue->q[i] = priority_queue->q[min_index];
+    priority_queue->q[min_index] = swapVar;
 }
 
-
-
-void swap(int *s, int i, int min_index ){
+void swap(int *s, int i, int min_index)
+{
 
     int swapVar;
 
     swapVar = s[i];
     s[i] = s[min_index];
     s[min_index] = swapVar;
-
 }
 
 int pq_parent(int n)
 {
-    if (n == 1) return(-1);
-    else return((int) n/2);
+    if (n == 1)
+        return (-1);
+    else
+        return ((int)n / 2);
 }
 
 /* implicitly take floor(n/2) */
 int pq_young_child(int n)
 {
-    return(2 * n);
+    return (2 * n);
 }
 
 int pq_insert(priority_queue *q, int x)
 {
     if (q->n >= PQ_SIZE)
-        printf("Warning: priority queue overflow insert x=%d\n",x);
-    else {
+        printf(""); //Warning: priority queue overflow insert x=%d\n, x
+    else
+    {
         q->n = (q->n) + 1;
-        q->q[ q->n ] = x;
+        q->q[q->n] = x;
         bubble_up(q, q->n);
     }
 }
 
 void bubble_up(priority_queue *q, int p)
 {
-    if (pq_parent(p) == -1) return; /* at root of heap, no parent */
+    if (pq_parent(p) == -1)
+        return; /* at root of heap, no parent */
 
-    if (q->q[pq_parent(p)] > q->q[p]) {
-        pq_swap(q,p,pq_parent(p));
+    if (q->q[pq_parent(p)] > q->q[p])
+    {
+        pq_swap(q, p, pq_parent(p));
         bubble_up(q, pq_parent(p));
     }
 }
@@ -150,35 +129,40 @@ void pq_init(priority_queue *q)
     q->n = 0;
 }
 
-void make_heap(priority_queue *q, int s[], int n)
+void make_heap(priority_queue *q, int s[], int n, int *operations)
 {
     int i;
     /* counter */
     pq_init(q);
 
-    for (i=0; i<n; i++)
+    for (i = 0; i < n; i++)
+    {
         pq_insert(q, s[i]);
+        *operations += 1;
+    }
 }
 
-int extract_min(priority_queue *q)
+int extract_min(priority_queue *q, int *operations)
 {
     int min = -1;
     /* minimum value */
 
     if (q->n <= 0)
-        printf("Warning: empty priority queue.\n");
-    else {
+        printf(""); //Warning: empty priority queue.\n
+    else
+    {
         min = q->q[1];
-        q->q[1] = q->q[ q->n ];
+        q->q[1] = q->q[q->n];
         q->n = q->n - 1;
-        bubble_down(q,1);
+        *operations += bubble_down(q, 1, (int)0);
     }
 
-    return(min);
+    return (min);
 }
 
-void bubble_down(priority_queue *q, int p)
+int bubble_down(priority_queue *q, int p, int operations)
 {
+
     int c;
     /* child index */
 
@@ -190,91 +174,109 @@ void bubble_down(priority_queue *q, int p)
 
     c = pq_young_child(p);
     min_index = p;
-    
+
     for (i = 0; i <= 1; i++)
+    {
         if ((c + i) <= q->n)
         {
             if (q->q[min_index] > q->q[c + i])
                 min_index = c + i;
         }
+        operations++;
+    }
+
     if (min_index != p)
     {
         pq_swap(q, p, min_index);
-        bubble_down(q, min_index);
+        operations += bubble_down(q, min_index, 0);
     }
+
+    return operations;
 }
 
-void heapsort(int s[], int n)
+int heapsort(int s[], int n)
 {
     int i;
+    int operations = 0;
     priority_queue q;
     /* counters */
     /* heap for heapsort */
 
-    make_heap(&q,s,n);
-    for (i=0; i<n; i++)
-        s[i] = extract_min(&q);
+    make_heap(&q, s, n, &operations);
+
+    for (i = 0; i < n; i++)
+    {
+        s[i] = extract_min(&q, &operations);
+        operations++;
+    }
+
+    return operations;
 }
 
-int simpleFindeMinimum(int * s, int n, int *operations){
+int simpleFindeMinimum(int *s, int n, int *operations)
+{
     int i;
     int minimum;
 
     minimum = s[0];
 
-    for(i = 1; i < n; i++){
-        
-        if( s[i] < minimum ){
+    for (i = 1; i < n; i++)
+    {
+
+        if (s[i] < minimum)
+        {
             minimum = s[i];
         }
     }
 
-   *operations += n; 
+    *operations += n;
 
-   return minimum;
+    return minimum;
 }
 
-int deleteMinimum(int * s, int start, int end, int minimum ){
+int deleteMinimum(int *s, int start, int end, int minimum)
+{
     int i;
     int swapVar;
 
-    for(i = start; i < end; ++i){
-    
-        if( minimum == s[i] ){
+    for (i = start; i < end; ++i)
+    {
+
+        if (minimum == s[i])
+        {
 
             swap(s, i, start);
-
         }
-
     }
 
     return i;
 }
 
-int SelectionSort(int *s, int * sortedS, int n){
-   int i;
-   int operations = 0;
+int SelectionSort(int *s, int *sortedS, int n)
+{
+    int i;
+    int operations = 0;
 
-   for( i = 0; i < n; ++i){
-        sortedS[i] = simpleFindeMinimum(s, n, &operations );
-        operations+=deleteMinimum(s, i, n, *sortedS);
+    for (i = 0; i < n; ++i)
+    {
+        sortedS[i] = simpleFindeMinimum(s, n, &operations);
+        operations += deleteMinimum(s, i, n, *sortedS);
         operations++;
-   }
+    }
 
-   return operations;
-
+    return operations;
 }
 
 int heap_compare(priority_queue *q, int i, int count, int x)
 {
     if ((count <= 0) || (i > q->n))
-        return(count);
+        return (count);
 
-    
-    if (q->q[i] < x) {
-        count = heap_compare(q, pq_young_child(i), count-1, x);
-        count = heap_compare(q, pq_young_child(i)+1, count, x);
+    if (q->q[i] < x)
+    {
+        count = heap_compare(q, pq_young_child(i), count - 1, x);
+        count = heap_compare(q, pq_young_child(i) + 1, count, x);
     }
 
-    return(count);
+    return (count);
 }
